29280d67809b1b6d77a38f4a503412938896f631
sushiomsky <harrybotya@gmail.com>
2017-06-25 06:21:12 +0200

    Activity & Fragments

---
+++ app/src/main/java/xyz/bitnaesser/multiplechoice/QuestionActivityFragment.java
@@ -0,0 +1,46 @@
+/*
+ * *
+ *  * ${PROJECT_NAME}
+ *  * Copyright (c) ${YEAR} Dennis Suchomsky <dennis.suchomsky@gmail.com>
+ *  *
+ *  *  This program is free software: you can redistribute it and/or modify
+ *  *  it under the terms of the GNU General Public License as published by
+ *  *  the Free Software Foundation, either version 3 of the License, or
+ *  *  (at your option) any later version.
+ *  *
+ *  *  This program is distributed in the hope that it will be useful,
+ *  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *  GNU General Public License for more details.
+ *  *
+ *  *  You should have received a copy of the GNU General Public License
+ *  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * *
+ *  @todo Keep it simple!
+ * /
+ */
+
+package xyz.bitnaesser.multiplechoice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class QuestionActivityFragment extends Fragment {
+
+    public QuestionActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_question, container, false);
+    }
+}
