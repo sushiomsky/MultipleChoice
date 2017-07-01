40280f03136573c8a51510a67514ffedc0ccb183
sushiomsky <harrybotya@gmail.com>
2017-07-01 10:58:32 +0200

    supersimple super bare

--- app/src/main/java/xyz/bitnaesser/multiplechoice/base/ServerAPI.java
+++ app/src/main/java/xyz/bitnaesser/multiplechoice/base/ServerAPI.java
@@ -1,23 +1,5 @@
-/*
- * *
- *  * ${PROJECT_NAME}
- *  * Copyright (c) ${YEAR} Dennis Suchomsky <dennis.suchomsky@gmail.com>
- *  *
- *  *  This program is free software: you can redistribute it and/or modify
- *  *  it under the terms of the GNU General Public License as published by
- *  *  the Free Software Foundation, either version 3 of the License, or
- *  *  (at your option) any later version.
- *  *
- *  *  This program is distributed in the hope that it will be useful,
- *  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  *  GNU General Public License for more details.
- *  *
- *  *  You should have received a copy of the GNU General Public License
- *  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * *
+/**
+ * { "type": "success", "value": [ { "id": 432, "joke": "Chuck Norris is the only man who has, literally, beaten the odds. With his fists.", "categories": [] }, { "id": 3, "joke": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants.", "categories": [] }, { "id": 122, "joke": "Chuck Norris is responsible for China's over-population. He hosted a Karate tournament in Beijing and all women within 1,000 miles became pregnant instantly.", "categories": [] } ]  }
  *  @todo Keep it simple!
  * /
  */
@@ -37,7 +19,7 @@ public interface ServerAPI {
     String ENDPOINT = "http://api.icndb.com";
 
     class Item {
-        @SerializedName("joke")
+        @SerializedName("question")
         public String text;
 
         @Override
