c5fb4e046d672105c14c64d4d8dd099a78caacb0
sushiomsky <harrybotya@gmail.com>
2017-07-03 22:43:34 +0200

    ServerApi working with remote json

--- app/src/main/java/xyz/bitnaesser/multiplechoice/base/ServerAPI.java
+++ app/src/main/java/xyz/bitnaesser/multiplechoice/base/ServerAPI.java
@@ -6,13 +6,48 @@
 
 package xyz.bitnaesser.multiplechoice.base;
 
+import android.text.Html;
+
+import com.google.gson.annotations.SerializedName;
+
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 
-public interface ServerAPI extends JsonApi {
+public interface ServerAPI {
 
-    String ENDPOINT = "http://bitnaesser.xyz";
+    String ENDPOINT = "https://api.myjson.com";
+	class Question {
+		@SerializedName("question")
+		public String text;
 
-    @GET("/question/random")
+		@SerializedName("id")
+		public String questionId;
+
+		@Override
+		public String toString() {
+			return Html.fromHtml(text).toString();
+		}
+
+		@SerializedName("answers")
+		public Answer[] answers;
+
+		public class Answer{
+			@SerializedName("answer")
+			public String text;
+
+			@SerializedName("id")
+			public String answerId;
+
+			@SerializedName("correct")
+			public String correct;
+		}
+	}
+
+	class Response {
+		@SerializedName("value")
+		public Question[] questions;
+	}
+
+    @GET("/bins/worp3")
     Observable<Response> getQuestions();
 }
